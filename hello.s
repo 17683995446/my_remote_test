dbca20816fc37611a8ec18b61d359f34facd85c7
shaoping <shaoping.wang@terapines.com>
2021-01-22 15:27:37 +0800

    添加hello.s文件用于远程测试

---
+++ hello.s
@@ -0,0 +1,28 @@
+	.text
+	.file	"hello.c"
+	.globl	main                            # -- Begin function main
+	.p2align	4, 0x90
+	.type	main,@function
+main:                                   # @main
+	.cfi_startproc
+# %bb.0:
+	pushq	%rax
+	.cfi_def_cfa_offset 16
+	movl	$.Lstr, %edi
+	callq	puts
+	xorl	%eax, %eax
+	popq	%rcx
+	.cfi_def_cfa_offset 8
+	retq
+.Lfunc_end0:
+	.size	main, .Lfunc_end0-main
+	.cfi_endproc
+                                        # -- End function
+	.type	.Lstr,@object                   # @str
+	.section	.rodata.str1.1,"aMS",@progbits,1
+.Lstr:
+	.asciz	"Hello world"
+	.size	.Lstr, 12
+
+	.ident	"clang version 11.0.0 (https://github.com/llvm/llvm-project.git 0160ad802e899c2922bc9b29564080c22eb0908c)"
+	.section	".note.GNU-stack","",@progbits
